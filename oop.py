4504ece126e89f0df695a430e81ddfdaf3a9392b
denis test <shdenis@gmail.com>
2024-06-02 23:34:12 +0500

    task 3

--- oop.py
+++ oop.py
@@ -16,16 +16,51 @@ class Student:
         else:
             return 'Ошибка'
 
+    def average_grade(self):
+        if not self.grades:
+            return 0
+        total_grades = [grade for grades in self.grades.values() for grade in grades]
+        return sum(total_grades) / len(total_grades)
+
+    def __str__(self):
+        avg_grade = self.average_grade()
+        courses_in_progress = ', '.join(self.courses_in_progress)
+        finished_courses = ', '.join(self.finished_courses)
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {avg_grade:.1f}\nКурсы в процессе изучения: {courses_in_progress}\nЗавершенные курсы: {finished_courses}'
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            return NotImplemented
+        return self.average_grade() < other.average_grade()
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def average_grade(self):
+        if not self.grades:
+            return 0
+        total_grades = [grade for grades in self.grades.values() for grade in grades]
+        return sum(total_grades) / len(total_grades)
+
+    def __str__(self):
+        avg_grade = self.average_grade()
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {avg_grade:.1f}'
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            return NotImplemented
+        return self.average_grade() < other.average_grade()
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -37,23 +72,36 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
-# Пример использования классов
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student.finished_courses += ['Введение в программирование']
 
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
 
 cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 9)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-
-print(best_student.grades)
 
 cool_lecturer = Lecturer('Cool', 'Lecturer')
 cool_lecturer.courses_attached += ['Python']
 
-best_student.rate_lecturer(cool_lecturer, 'Python', 9)
+best_student.rate_lecturer(cool_lecturer, 'Python', 10)
 best_student.rate_lecturer(cool_lecturer, 'Python', 8)
 
-print(cool_lecturer.grades)
+print(best_student)
+print(cool_reviewer)
+print(cool_lecturer)
+
+another_student = Student('John', 'Doe', 'male')
+another_student.courses_in_progress += ['Python']
+another_student.finished_courses += ['Введение в программирование']
+cool_reviewer.rate_hw(another_student, 'Python', 8)
+cool_reviewer.rate_hw(another_student, 'Python', 7)
+cool_reviewer.rate_hw(another_student, 'Python', 9)
+
+print(another_student)
+
+print(best_student > another_student)
+print(best_student < another_student)
